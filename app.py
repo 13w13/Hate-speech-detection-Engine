d23f04d76eaca8062c125861fba00084c3e94f67
13w13 <s.weiss@tbs-education.org>
2021-02-21 21:23:10 +0100

    add

--- app.py
+++ app.py
@@ -131,7 +131,7 @@ def home():
 
 def predict():
     if request.method == 'POST':
-        tweet = [str(x) for x in request.values.get()]
+        tweet = [str(x) for x in request.form()]
         
         #app.logger.info(tweet)
 
