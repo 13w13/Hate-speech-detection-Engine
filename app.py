72f3a737688c29df35d53618cd5ed24f065864f7
13w13 <s.weiss@tbs-education.org>
2021-02-21 12:01:59 +0100

    add

--- app.py
+++ app.py
@@ -1,36 +1,34 @@
 #import libraries
-import logging
-from flask import Flask, request, jsonify, render_template
-import sys
-import fastai
-from fastai.callbacks import TrainingPhase, GeneralScheduler
-import fastprogress # a remettre
-from fastprogress import master_bar, progress_bar #a remettre 
+#import logging
+from flask import Flask, request, render_template
+#import sys
+#import fastai
+#from fastai.callbacks import TrainingPhase, GeneralScheduler
+#import fastprogress # a remettre
+#from fastprogress import master_bar, progress_bar #a remettre 
 import numpy as np
 import pandas as pd
 import os
-import gc
+#import gc
 import random
 from tqdm._tqdm_notebook import tqdm_notebook as tqdm
 #from keras.preprocessing import text, sequence
 import torch
 from torch import nn
-from torch.utils import data
-from torch.nn import functional as F
-import torch.utils.data
+#from torch.utils import data
+#from torch.nn import functional as F
+#import torch.utils.data
+from torch.utils.data import DataLoader, TensorDataset
 from tqdm import tqdm
-from pytorch_pretrained_bert import BertTokenizer, BertForSequenceClassification, BertAdam
-from pytorch_pretrained_bert import BertConfig
-from nltk.tokenize.treebank import TreebankWordTokenizer
-from gensim.models import KeyedVectors
-
+from pytorch_pretrained_bert import BertTokenizer, BertForSequenceClassification, BertAdam, BertConfig
+#from nltk.tokenize.treebank import TreebankWordTokenizer
+#from gensim.models import KeyedVectors
 
 #Initialize the flask App
 app = Flask(__name__)
-
-handler = logging.FileHandler("test.log")  # Create the file logger
-app.logger.addHandler(handler)             # Add it to the built-in logger
-app.logger.setLevel(logging.DEBUG) 
+#handler = logging.FileHandler("test.log")  # Create the file logger
+#app.logger.addHandler(handler)             # Add it to the built-in logger
+#app.logger.setLevel(logging.DEBUG) 
 
 device=torch.device('cpu')
 MAX_SEQUENCE_LENGTH = 300 ## 220 in training
@@ -67,79 +65,15 @@ def convert_lines(example, max_seq_length,tokenizer):
         all_tokens.append(one_token)
     return np.array(all_tokens)
 
-def is_interactive():
-    return 'SHLVL' not in os.environ
-
 def seed_everything(seed=123):
     random.seed(seed)
     os.environ['PYTHONHASHSEED'] = str(seed)
     np.random.seed(seed)
     torch.manual_seed(seed)
     torch.backends.cudnn.deterministic = True
-    
-def get_coefs(word, *arr):
-    return word, np.asarray(arr, dtype='float32')
-
-def load_embeddings(path):
-    #with open(path,'rb') as f:
-    emb_arr = KeyedVectors.load(path)
-    return emb_arr
 
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
-
-class SpatialDropout(nn.Dropout2d):
-    def forward(self, x):
-        x = x.unsqueeze(2)    # (N, T, 1, K)
-        x = x.permute(0, 3, 2, 1)  # (N, K, 1, T)
-        x = super(SpatialDropout, self).forward(x)  # (N, K, 1, T), some features are masked
-        x = x.permute(0, 3, 2, 1)  # (N, T, 1, K)
-        x = x.squeeze(2)  # (N, T, K)
-        return x
-
-def handle_contractions(x):
-    x = tokenizer.tokenize(x)
-    return x
-
-def fix_quote(x):
-    x = [x_[1:] if x_.startswith("'") else x_ for x_ in x]
-    x = ' '.join(x)
-    return x
-
-def reduce_mem_usage(df):
-    start_mem = df.memory_usage().sum() / 1024**2
-    print('Memory usage of dataframe is {:.2f} MB'.format(start_mem))
-    
-    for col in df.columns:
-        col_type = df[col].dtype
-        
-        if col_type != object:
-            c_min = df[col].min()
-            c_max = df[col].max()
-            if str(col_type)[:3] == 'int':
-                if c_min > np.iinfo(np.int8).min and c_max < np.iinfo(np.int8).max:
-                    df[col] = df[col].astype(np.int8)
-                elif c_min > np.iinfo(np.int16).min and c_max < np.iinfo(np.int16).max:
-                    df[col] = df[col].astype(np.int16)
-                elif c_min > np.iinfo(np.int32).min and c_max < np.iinfo(np.int32).max:
-                    df[col] = df[col].astype(np.int32)
-                elif c_min > np.iinfo(np.int64).min and c_max < np.iinfo(np.int64).max:
-                    df[col] = df[col].astype(np.int64)  
-            else:
-                if c_min > np.finfo(np.float16).min and c_max < np.finfo(np.float16).max:
-                    df[col] = df[col].astype(np.float16)
-                elif c_min > np.finfo(np.float32).min and c_max < np.finfo(np.float32).max:
-                    df[col] = df[col].astype(np.float32)
-                else:
-                    df[col] = df[col].astype(np.float64)
-        else:
-            df[col] = df[col].astype('category')
-
-    end_mem = df.memory_usage().sum() / 1024**2
-    print('Memory usage after optimization is: {:.2f} MB'.format(end_mem))
-    print('Decreased by {:.1f}%'.format(100 * (start_mem - end_mem) / start_mem))
-    
-    return df
 
 def predict_words(tweet): 
 
@@ -151,8 +85,10 @@ def predict_words(tweet):
   X_test = convert_lines(test_input_df["comment_text"].fillna("DUMMY_VALUE"), MAX_SEQUENCE_LENGTH, tokenizer)
   test_preds = np.zeros((len(X_test)))
 
-  test = torch.utils.data.TensorDataset(torch.tensor(X_test, dtype=torch.long))
-  test_loader = torch.utils.data.DataLoader(test, batch_size=512, shuffle=False)
+  #test = torch.utils.data.TensorDataset(torch.tensor(X_test, dtype=torch.long))
+  test = TensorDataset(torch.tensor(X_test, dtype=torch.long))
+  #test_loader = torch.utils.data.DataLoader(test, batch_size=512, shuffle=False)
+  test_loader = DataLoader(test, batch_size=512, shuffle=False)
   tk0 = tqdm(test_loader)
   for i, (x_batch,) in enumerate(tk0):
     pred = model(x_batch.to(device), attention_mask=(x_batch > 0).to(device), labels=None)
@@ -167,18 +103,7 @@ def predict_words(tweet):
 
   return float(submission_bert['prediction'].values)
 
-if not is_interactive():
-    def nop(it, *a, **k):
-        return it
-
-    tqdm = nop
-
-    fastprogress.fastprogress.NO_BAR = True
-    #master_bar, progress_bar = force_console_behavior()
-    fastai.basic_train.master_bar, fastai.basic_train.progress_bar = master_bar, progress_bar
-
 seed_everything()
-
 
 model = BertForSequenceClassification(bert_config, num_labels=1)
 model.load_state_dict(torch.hub.load_state_dict_from_url("https://www.googleapis.com/drive/v3/files/1RYFMsASHW7a92qa7zW296zgnToRQFeb5?alt=media&key=AIzaSyA0OHTKp3e0TvdIyua79c8jH_v6WBmGEKI", map_location=torch.device('cpu')))
@@ -204,7 +129,7 @@ def predict():
     For rendering results on HTML GUI
     '''
     tweet = [str(x) for x in request.form.values()]
-    app.logger.info(tweet)
+    #app.logger.info(tweet)
 
     ############
 
@@ -232,8 +157,8 @@ def predict():
     return render_template('index.html', prediction_text='Prediction is :{}'.format(prediction))
 
 if __name__ == '__main__':
-    app.debug = True
+    #app.debug = True
     port = int(os.environ.get("PORT", 80))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='127.0.0.1', port=port, debug=True)
 
     
