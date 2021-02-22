f74a6eab972399ac3a231657f0ec2ade6fc550f2
13w13 <s.weiss@tbs-education.org>
2021-02-22 12:17:09 +0100

    reladcach

--- app.py
+++ app.py
@@ -167,6 +167,6 @@ def predict():
 if __name__ == '__main__':
     app.debug = True
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='127.0.0.1', port=port, debug=True)
 
     
