d2b22e77bf30ca4f83b621f4a9e316e68e04e4c4
mickey6934 <settelen@etud.insa-toulouse.fr>
2021-02-23 16:20:36 +0100

    test4

--- app.py
+++ app.py
@@ -210,6 +210,6 @@ def get_predict_result(job_key):
 if __name__ == '__main__':
     app.debug = True
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='127.0.0.1', port=port, debug=True)
 
     
