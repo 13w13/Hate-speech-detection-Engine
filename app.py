42d923d29c00d45d4befd6414872554a8ca6c06f
mickey6934 <settelen@etud.insa-toulouse.fr>
2021-02-27 15:38:07 +0100

    merge main

--- app.py
+++ app.py
@@ -1,41 +1,21 @@
-#import libraries
-#import logging
-from flask import Flask, request, render_template
-#import sys
-#import fastai
-#from fastai.callbacks import TrainingPhase, GeneralScheduler
-#import fastprogress # a remettre
-#from fastprogress import master_bar, progress_bar #a remettre 
+from flask import Flask, request, jsonify, render_template, Response, make_response
 import numpy as np
 import pandas as pd
 import os
-#import gc
 import random
 from tqdm._tqdm_notebook import tqdm_notebook as tqdm
-#from keras.preprocessing import text, sequence
 import torch
 from torch import nn
-#from torch.utils import data
-#from torch.nn import functional as F
-#import torch.utils.data
 from torch.utils.data import DataLoader, TensorDataset
 from tqdm import tqdm
 from pytorch_pretrained_bert import BertTokenizer, BertForSequenceClassification, BertAdam, BertConfig
-#from nltk.tokenize.treebank import TreebankWordTokenizer
-#from gensim.models import KeyedVectors
-import time
+import logging
 
 #Initialize the flask App
 #app = Flask(__name__)
 #handler = logging.FileHandler("test.log")  # Create the file logger
 #app.logger.addHandler(handler)             # Add it to the built-in logger
 #app.logger.setLevel(logging.DEBUG) 
-
-from rq import Queue
-from rq.job import Job
-from rq import get_current_job
-from worker import conn
-
 
 device=torch.device('cpu')
 MAX_SEQUENCE_LENGTH = 300 ## 220 in training
@@ -100,11 +80,8 @@ def predict_words(tweet):
     test_loader = DataLoader(test, batch_size=512, shuffle=False)
     #tk0 = tqdm(test_loader)
 
-
-
     for i, (x_batch,) in enumerate(test_loader):
         pred = model(x_batch, attention_mask=(x_batch > 0), labels=None)
-        return(pred)
         test_preds[i * 512:(i + 1) * 512] = pred[:, 0].detach().cpu().squeeze().numpy()
 
     test_pred = torch.sigmoid(torch.tensor(test_preds)).numpy().ravel()  
@@ -119,33 +96,21 @@ def predict_words(tweet):
 seed_everything()
 
 model = BertForSequenceClassification(bert_config, num_labels=1)
-torch.hub.load_state_dict_from_url("https://www.dropbox.com/s/4320po4qph1lrx6/bert_pytorch.bin?dl=1", model_dir="./",map_location=torch.device('cpu'))
+#torch.hub.load_state_dict_from_url("https://www.dropbox.com/s/4320po4qph1lrx6/bert_pytorch.bin?dl=1", model_dir="./",map_location=torch.device('cpu'))
+model.load_state_dict(torch.hub.load_state_dict_from_url("https://www.googleapis.com/drive/v3/files/1RYFMsASHW7a92qa7zW296zgnToRQFeb5?alt=media&key=AIzaSyA0OHTKp3e0TvdIyua79c8jH_v6WBmGEKI", model_dir="input/arti-bert-inference/bert",map_location=torch.device('cpu')))
 #model.load_state_dict()
 for param in model.parameters():
     param.requires_grad = False
 model.eval()
 
+#Initialize the flask App
 app = Flask(__name__)
-q = Queue(connection=conn)
 
-def get_predict_result(job_key):
-    #job_key = "rq:worker:7e8f878d9a8c42029261a26e056bd742"
-    job_key = job_key.replace("rq:job:", "")
-    #return render_template('index.html', prediction_text='Prediction is :{}'.format(job_key))
-    job = Job.fetch(job_key, connection=conn)
-    compteur=0
+#model = pickle.load(open('model.pkl', 'rb')) #changer par le bin
 
-    while(not job.is_finished):
-        #render_template('index.html', prediction_text='Prediction is :{}'.format(str("toto"))) 
-        time.sleep(1)
-        #compteur = compteur+1
-        #if(compteur == 20):
-        #break
-    #if(compteur == 20):
-    #    return render_template('index.html', prediction_text='Prediction is :{}'.format(str("toto")))
-    #else:
-    #    return render_template('index.html', prediction_text='Prediction is :{}'.format(str(job.result)))
-    return render_template('index.html', prediction_text='Prediction is :{}'.format(str(job.result)))
+handler = logging.FileHandler("test.log")  # Create the file logger
+app.logger.addHandler(handler)             # Add it to the built-in logger
+app.logger.setLevel(logging.DEBUG) 
 
 #default page of our web-app
 @app.route('/')
@@ -154,73 +119,58 @@ def home():
     return render_template('index.html')
 
 #To use the predict button in our web-app
-@app.route('/predict',methods=["POST", "GET"])
-
+@app.route('/predict',methods=['POST'])
 def predict():
-    if request.method == 'POST':
-        tweet = [str(x) for x in request.form.values()]
-        
-        #app.logger.info(tweet)
+    tweet = [str(x) for x in request.form.values()]
+    #app.logger.info(tweet)
 
-        ############
+    # Input 
+    # ##faire les memes operations de test que dans le notebook 
 
-        # Input 
+    prediction = "indetermined"
+    prob_prediction = predict_words(tweet[0])
 
-        ##faire les memes operations de test que dans le notebook 
-
-        #return(int_features)
-
-        #final_features = [np.array(int_features)]
-        #prediction = model.predict(final_features)
-
-        #output = round(prediction[0], 2)
-
-        prediction = "bonjour"
-        prob_prediction = []
-
-        #prob_prediction = q.enqueue(predict_words, (tweet[0]))
-        
-        #job = q.enqueue(predict_words, (tweet[0]))
-
-        job = q.enqueue_call(
-            func=predict_words, args=(tweet[0],), result_ttl=50000
-        )
-        print(job.get_id())
-
-        """
-        if prob_prediction >= 0.6: 
-            prediction = "Insult "
-        elif prob_prediction >= 0.4 and prob_prediction < 0.6: 
-            prediction = "Neutral "
-        else:
-            prediction = "Non toxic "
-
-        """
-        #time.sleep(5)
-
-        return get_predict_result(job.key.decode("utf-8"))
-        #b'rq:job:61dd5aaa-3ac3-41d7-9f91-378b20c544b8' 
-
-        #return render_template('index.html', prediction_text='Prediction is :{}'.format(job.key.decode("utf-8")))
-
-'''        
-@app.route("/predict/<job_key>", methods=['GET'])
-def get_predict_result(job_key):
-    #job_key = "rq:worker:7e8f878d9a8c42029261a26e056bd742"
-    job_key = job_key.replace("rq:job:", "")
-    #return render_template('index.html', prediction_text='Prediction is :{}'.format(job_key))
-    job = Job.fetch(job_key, connection=conn)
-
-    if(not job.is_finished):
-        return "Not yet", 202
+    if prob_prediction >= 0.6: 
+        prediction = "Insult "
+    elif prob_prediction >= 0.4 and prob_prediction < 0.6: 
+        prediction = "Neutral "
     else:
-        #return str(job.result), 200
-        return render_template('index.html', prediction_text='Prediction is :{}'.format(str(job.result))), 200
-'''
+        prediction = "Non toxic "
+
+    return render_template('predict.html', prediction_text='Is it an hate message? :{}'.format(prediction))
+
+@app.route('/predict_csv',methods=['POST'])
+def predict_csv():
+    return render_template('predict.html', prediction_text='Is it an hate message? :{}'.format('tutu'))
+
+@app.route('/predict_csv/file',methods=['POST'])
+def predict_csv_file():
+    '''
+    For rendering results on HTML GUI
+    '''
+    # get the uploaded file
+    uploaded_file = request.files['file']
+    csvData = pd.read_csv(uploaded_file, header=None)
+    csvData = csvData.rename(columns={0:'tweet'})
+    csvData['pred'] = None
+    
+    for i in range(len(csvData)): 
+        #csvData['pred'][i] = 0.5
+        csvData['pred'][i] = predict_words(csvData.loc[i].values[0])
+        # #print(tweet_df.loc[i].values[0] + " " + str(predict_words(tweet_df.loc[i].values[0])))
+
+    #app.logger.info(csvData)
+
+    #output = round(prediction[0], 2)
+    #output = "toto"
+
+    resp = make_response(csvData.to_csv())
+    resp.headers["Content-Disposition"] = "attachment; filename=export.csv"
+    resp.headers["Content-Type"] = "text/csv"
+
+    return resp
 
 if __name__ == '__main__':
     app.debug = True
     port = int(os.environ.get("PORT", 5000))
     app.run(host='127.0.0.1', port=port, debug=True)
-
-    
